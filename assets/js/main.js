726d1e6a8d7526e9710acf34390fbace356a3d7e
mayulsaka <mayumilhotesakaguchi@yahoo.co.jp>
2024-12-22 04:15:14 +0900

    L5lp1__241220(FBに基づく修正いったん完了)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,13 @@
 // ---------- ハンバーガーメニュー ---------- 
-// const ham = $('#js-hamburger');
-// const nav = $('#js-nav');
-// ham.on('click', function () {
-//   ham.toggleClass('active');
-//   nav.toggleClass('active');
-// });
-
-// ---------- ハンバーガーメニュー ---------- 
 const ham = $('#js-hamburger');
 const nav = $('#js-nav');
 
-// ハンバーガーメニューのクリックイベント
 ham.on('click', function () {
     ham.toggleClass('active');
     nav.toggleClass('active');
 });
 
-// ウィンドウサイズ変更時に実行
 $(window).on('resize', function () {
-    if ($(window).width() >= 760) {
-        // ハンバーガーメニューを閉じる
         ham.removeClass('active');
         nav.removeClass('active');
-    }
 });
