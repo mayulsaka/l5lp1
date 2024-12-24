1224329a1391d470cf727cb235f830b68ee88669
mayulsaka <mayumilhotesakaguchi@yahoo.co.jp>
2024-12-25 08:51:41 +0900

    l5lp1_241225v2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 // ハンバーガーメニュー
 
-=======
-// ---------- ハンバーガーメニュー ---------- 
->>>>>>> 726d1e6a8d7526e9710acf34390fbace356a3d7e
 const ham = $('#js-hamburger');
 const nav = $('#js-nav');
 
@@ -11,10 +7,6 @@ ham.on('click', function () {
     ham.toggleClass('active');
     nav.toggleClass('active');
 });
-<<<<<<< HEAD
-=======
-
->>>>>>> 726d1e6a8d7526e9710acf34390fbace356a3d7e
 $(window).on('resize', function () {
         ham.removeClass('active');
         nav.removeClass('active');
